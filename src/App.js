88bb76fda9c89e0ee3219d5a3e5a0e43457f76c7
Shouvick Halder <deeprony7@gmail.com>
2020-11-08 21:21:10 +0530

    44. function based approach

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: '28' },
       { name: 'Shouvick', age: '99' },
       { name: 'Joe', age: '78' }
     ]
-  }
+  })
 
-  switchNameHandler = () => {
-    this.setState({
+  const [otherState, setOtherState] = useState("some other value")
+
+  console.log(personsState, otherState)
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Raju', age: '28' },
         { name: 'Shouvick', age: '99' },
@@ -21,17 +26,17 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I am a React App!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: coding</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi I am a React App!</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: coding</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+
 }
 
-export default App;
+export default app;
+
