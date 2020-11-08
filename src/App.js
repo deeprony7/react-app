9acb7c781f2e3e4616ce9bd6262f485d1e011172
Shouvick Halder <deeprony7@gmail.com>
2020-11-08 23:42:57 +0530

    section 3 assignment

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import UserInput from './UserInput/UserInput'
+import UserOutput from './UserOutput/UserOutput'
 
 class App extends Component {
   state = {
-    persons: [
-      { name: 'Max', age: '28' },
-      { name: 'Shouvick', age: '99' },
-      { name: 'Joe', age: '78' }
-    ]
+    username: "suoermax"
   }
 
-  switchNameHandler = (newName) => {
+  inputChangeHandler = (event) => {
     this.setState({
-      persons: [
-        { name: newName, age: '28' },
-        { name: 'Shouvick', age: '99' },
-        { name: 'Joe', age: '78' }
-      ]
-    })
-  }
-
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Max", age: '28' },
-        { name: event.target.value, age: '99' },
-        { name: 'Joe', age: '78' }
-      ]
+      username: event.target.value
     })
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
 
     return (
       <div className="App">
-        <h1>Hi I am a React App!</h1>
-        <button 
-        style={style}
-        onClick={() => this.switchNameHandler("Kane")}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
-        click={this.switchNameHandler.bind(this, "Ricardo")}
-        changed={this.nameChangedHandler}>My hobbies: coding</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <h1>Assignment - 1</h1>
+        <UserInput
+          changed={this.inputChangeHandler}
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="JohnLegend" />
       </div>
     );
   }
