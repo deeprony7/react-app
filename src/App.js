8f63696f1c1c6ec1058b95a1eda37f870cede2b2
Shouvick Halder <deeprony7@gmail.com>
2020-11-09 23:00:09 +0530

    54. handling dynamc content the JS way/ preferred way

--- src/App.js
+++ src/App.js
@@ -48,22 +48,27 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div >
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Ricardo")}
+          changed={this.nameChangedHandler}>My hobbies: coding</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div> 
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi I am a React App!</h1>
         <button
           style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-        { this.state.showPersons ?
-          <div >
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Ricardo")}
-              changed={this.nameChangedHandler}>My hobbies: coding</Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div> : null
-        }
-
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
       </div>
     );
   }
