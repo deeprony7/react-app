c48b0d559577948fe9e2f4a6ff9e9833d4636266
Shouvick Halder <deeprony7@gmail.com>
2020-11-11 13:24:25 +0530

    removed styled component from app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
 import './App.css';
 import Person from './Person/Person'
-
-const StyleButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black
-  }
-`
 
 class App extends Component {
   state = {
@@ -113,8 +98,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi I am a React App!</h1>
         <p className={classes.join(' ')}>This is a paragraph</p>
-        <StyleButton alt={this.state.showPersons}
-          onClick={this.togglePersonsHandler}>Toggle Persons</StyleButton>
+        <button className="button"
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
